52fa4585cb36d93fbe2020f41c932ed4cd9aad53
muppidathibecse <muppidathi.m@softsuave.org>
2025-10-24 14:02:48 +0530

    Added Brands

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
 import { Navbar } from '../navbar/navbar';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [Navbar],
+  imports: [Navbar, CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home {
+  images: string[] = [
+    'assets/brands/Adidas.jpg',
+    'assets/brands/A.webp',
+    'assets/brands/Puma.jpg',
+    'assets/brands/Nb.jpg',
+    'assets/brands/Hm.jpg',
+    'assets/brands/Nike.jpg',
+  ];
+
+  currentIndex: number = 0;
+  direction: 'next' | 'prev' = 'next'; // to know slide direction
+
+  prev() {
+    this.direction = 'prev';
+    this.currentIndex = this.currentIndex === 0 ? this.images.length - 1 : this.currentIndex - 1;
+  }
+
+  next() {
+    this.direction = 'next';
+    this.currentIndex = this.currentIndex === this.images.length - 1 ? 0 : this.currentIndex + 1;
+  }
+}
