3426d21d8ce254e1610574850d8aeb365095e72d
MUPPIDATHI-82 <muppidathibecse@gmail.com>
2025-10-26 01:24:10 +0530

    Added Men and Women

---
+++ src/app/components/icons/star/star.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-star',
+  template: `<svg viewBox="0 0 24 24" class="w-5 h-5" [class]="color">
+    <path
+      fill="currentColor"
+      d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434
+                        c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425
+                        L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527
+                        c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
+    />
+  </svg>`,
+})
+export class StarComponent {
+  @Input() color = 'text-gray-800';
+}
--- src/app/components/women/women.ts
+++ src/app/components/women/women.ts
@@ -1,12 +1,106 @@
 import { Component } from '@angular/core';
 import { Navbar } from '../navbar/navbar';
+import { StarComponent } from '../icons/star/star';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-women',
-  imports: [Navbar],
+  imports: [Navbar, StarComponent, CommonModule],
   templateUrl: './women.html',
-  styleUrl: './women.css'
+  styleUrl: './women.css',
 })
 export class Women {
-
+  //https://www.linenclub.com/men-wear/men/top-wear/shirts?srsltid=AfmBOopTVDxRWt5ZR-Wmw8Puz860U9TYJx0oRkfP7Z1Hiclk_dtngOdl
+  MenDetails = [
+    {
+      src: 'assets/womens/W1.jpg',
+      brand: 'NETPLAY',
+      name: 'Men Regular Fit Shirt',
+      rs: 1700,
+      oldrs: 2000,
+      off: 45,
+      rev: 1,
+    },
+    {
+      src: 'assets/womens/W2.avif',
+      brand: 'NETPLAY',
+      name: 'Men Regular Fit Shirt',
+      rs: 1700,
+      oldrs: 2000,
+      off: 45,
+      rev: 2,
+    },
+    {
+      src: 'assets/womens/W3.jpg',
+      brand: 'NETPLAY',
+      name: 'Men Regular Fit Shirt',
+      rs: 1700,
+      oldrs: 2000,
+      off: 45,
+      rev: 3,
+    },
+    {
+      src: 'assets/womens/W4.avif',
+      brand: 'NETPLAY',
+      name: 'Men Regular Fit Shirt',
+      rs: 1700,
+      oldrs: 2000,
+      off: 45,
+      rev: 4,
+    },
+    {
+      src: 'assets/womens/W5.avif',
+      brand: 'NETPLAY',
+      name: 'Men Regular Fit Shirt',
+      rs: 1700,
+      oldrs: 2000,
+      off: 45,
+      rev: 5,
+    },
+    {
+      src: 'assets/womens/W6.jpg',
+      brand: 'NETPLAY',
+      name: 'Men Regular Fit Shirt',
+      rs: 1700,
+      oldrs: 2000,
+      off: 45,
+      rev: 1,
+    },
+    {
+      src: 'assets/womens/W7.jpg',
+      brand: 'NETPLAY',
+      name: 'Men Regular Fit Shirt',
+      rs: 1700,
+      oldrs: 2000,
+      off: 45,
+      rev: 2,
+    },
+    {
+      src: 'assets/womens/W8.avif',
+      brand: 'NETPLAY',
+      name: 'Men Regular Fit Shirt',
+      rs: 1700,
+      oldrs: 2000,
+      off: 45,
+      rev: 3,
+    },
+    {
+      src: 'assets/womens/W9.jpg',
+      brand: 'NETPLAY',
+      name: 'Men Regular Fit Shirt',
+      rs: 1700,
+      oldrs: 2000,
+      off: 45,
+      rev: 4,
+    },
+    {
+      src: 'assets/womens/W10.avif',
+      brand: 'NETPLAY',
+      name: 'Men Regular Fit Shirt',
+      rs: 1700,
+      oldrs: 2000,
+      off: 45,
+      rev: 5,
+    },
+  ];
 }
