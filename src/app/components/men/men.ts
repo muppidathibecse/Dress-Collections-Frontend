7e3ff8cdd1ee40710a71382c425d4527ec2df10d
MUPPIDATHI-82 <muppidathibecse@gmail.com>
2025-10-25 23:41:08 +0530

    Add Blue Print in Men Component

--- src/app/components/men/men.ts
+++ src/app/components/men/men.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
 import { Navbar } from '../navbar/navbar';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-men',
-  imports: [Navbar],
+  imports: [Navbar, CommonModule],
   templateUrl: './men.html',
-  styleUrl: './men.css'
+  styleUrl: './men.css',
 })
 export class Men {
-
+  MenDetails = [
+    { brand: 'NETPLAY', name: 'Men Regular Fit Shirt', rs: 1700, oldrs: 2000, off: 45, rev: 4 },
+    {
+      brand: 'NETPLAY',
+      name: 'Men Regular Fit Shirt hgfhgfd effd',
+      rs: 1700,
+      oldrs: 2000,
+      off: 45,
+      rev: 4,
+    },
+    { brand: 'NETPLAY', name: 'Men ', rs: 1700, oldrs: 2000, off: 45, rev: 4 },
+    { brand: 'NETPLAY', name: 'Men Regular Fit Shirt', rs: 1700, oldrs: 2000, off: 45, rev: 4 },
+    { brand: 'NETPLAY', name: 'Men Regular Fit Shirt', rs: 17, oldrs: 200, off: 45, rev: 4 },
+    { brand: 'NETPLAY', name: 'Men Regular Fit Shirt', rs: 1700, oldrs: 2000, off: 45, rev: 4 },
+    { brand: 'NETPLAY', name: 'Men Regular Fit Shirt', rs: 17, oldrs: 200, off: 45, rev: 4 },
+    { brand: 'NETPLAY', name: 'Men Regular Fit Shirt', rs: 1700, oldrs: 2000, off: 45, rev: 4 },
+    { brand: 'NETPLAY', name: 'Men Regular Fit Shirt', rs: 17, oldrs: 200, off: 45, rev: 4 },
+    { brand: 'NETPLAY', name: 'Men Regular Fit Shirt', rs: 1700, oldrs: 2000, off: 45, rev: 4 },
+    { brand: 'NETPLAY', name: 'Men Regular Fit Shirt', rs: 17, oldrs: 200, off: 45, rev: 4 },
+    { brand: 'NETPLAY', name: 'Men Regular Fit Shirt', rs: 1700, oldrs: 2000, off: 45, rev: 4 },
+    { brand: 'NETPLAY', name: 'Men Regular Fit Shirt', rs: 17, oldrs: 200, off: 45, rev: 4 },
+    { brand: 'NETPLAY', name: 'Men Regular Fit Shirt', rs: 1700, oldrs: 2000, off: 45, rev: 4 },
+    { brand: 'NETPLAY', name: 'Men Regular Fit Shirt', rs: 17, oldrs: 200, off: 45, rev: 4 },
+  ];
 }
