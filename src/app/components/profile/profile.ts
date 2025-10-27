d3fe8168ead0eae40cbc110029ef97cc6b27c8aa
muppidathibecse <muppidathi.m@softsuave.org>
2025-10-27 19:58:46 +0530

    Completed the FD

--- src/app/components/profile/profile.ts
+++ src/app/components/profile/profile.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Navbar } from '../navbar/navbar';
+import { RightArrow } from '../icons/right-arrow/right-arrow';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [Navbar],
+  imports: [Navbar, RightArrow],
   templateUrl: './profile.html',
   styleUrl: './profile.css',
 })
